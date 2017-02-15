80525d4d07b5cb3fa51556fb3408b7f43b2d0790
Chris <electronicsleep@gmail.com>
2017-02-15 00:15:35 -0800

    Setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="clp",
+    version='0.1',
+    py_modules=['clp'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        clp=clp:cli
+    ''',
+)
