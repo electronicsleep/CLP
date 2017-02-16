09bc16021cf87cef622fabe95fb16be78b3b8561
Chris <electronicsleep@gmail.com>
2017-02-15 20:21:55 -0800

    Iterate idea example

--- clp.py
+++ clp.py
@@ -3,45 +3,47 @@ import subprocess
 
 @click.command()
 @click.option('--count', default=1, help='Number of iterations.')
-@click.option('--name', prompt='Your name', help='Who are you?')
 @click.option('--verbose', is_flag=True, help='Print verbose output.')
 
-def hello(count, name, verbose):
-    """Simple program that greets NAME for a total of COUNT times."""
-
-    pipe = ''
-    line_num = 0
-
-    print "#" * 10 
-
-    if verbose:
-        click.echo("We are in the verbose mode.")
-
-    pipe = subprocess.Popen('cat data.txt', shell=True, stdout=subprocess.PIPE)
-    for line in pipe.stdout:
-        print(line.strip())
-        line_num += 1
-
-    print 'lines: ' + str(line_num)
-
-    print "#" * 10 
-
-    line_num = 0
-    pipe = subprocess.Popen('ls -ltra', shell=True, stdout=subprocess.PIPE)
-    for line in pipe.stdout:
-        print(line.strip())
-        if '.py' in line:
-            print "found python file"
-        if '.txt' in line:
-            print "found txt file"
-        line_num += 1
-
-    print 'lines: ' + str(line_num)
-
-    print "#" * 10 
+def main(count, verbose):
+    """Program that runs a specfic command for count iterations."""
 
     for x in range(count):
-        click.echo('Hello %s!' % name)
+
+        pipe = ''
+        line_num = 0
+
+        if verbose:
+            click.echo("We are in the verbose mode.")
+
+        pipe = subprocess.Popen('cat data.txt', shell=True, stdout=subprocess.PIPE)
+        for line in pipe.stdout:
+            if 'bar' in line:
+                print("found bar")
+            print(line.strip())
+            line_num += 1
+
+
+        print 'lines: ' + str(line_num)
+
+        print "#" * 10 
+
+        line_num = 0
+        pipe = subprocess.Popen('ls -ltra', shell=True, stdout=subprocess.PIPE)
+        for line in pipe.stdout:
+            print(line.strip())
+            line_num += 1
+            if verbose:
+                if '.py' in line:
+                    print "found python file"
+                if '.txt' in line:
+                    print "found txt file"
+
+        print 'lines: ' + str(line_num)
+
+    print 'RUN:',
+    print x + 1
+
 
 if __name__ == '__main__':
-    hello()
+    main()
