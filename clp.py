42b890f1fe8b9f9a4b713d849b1538abb2c3f86d
Chris <electronicsleep@gmail.com>
2017-03-24 18:45:38 -0700

    more examples

--- clp.py
+++ clp.py
@@ -8,12 +8,14 @@ import six
 @click.option('-c', '--count', default=1, help='Number of iterations.')
 @click.option('-v', '--verbose', is_flag=True, help='Print verbose output.')
 
+
 def main(count, verbose):
     """ Example program that runs a specfic command a number of times and scans output. """
 
+    found_list = []
+
     for x in range(count):
 
-        pipe = ''
         line_num = 0
 
         if verbose:
@@ -25,6 +27,7 @@ def main(count, verbose):
                 line_num += 1
                 if 'bar' in line:
                     print("found bar")
+                    found_list.append("found bar")
                 print(line.strip())
 
         print('lines: ' + str(line_num))
@@ -36,13 +39,21 @@ def main(count, verbose):
         for line in pipe.stdout:
             print(line.strip())
             line_num += 1
-            if verbose:
-                if '.py' in line:
+
+            if '.py' in line:
+                if verbose:
                     print("found python file")
-                if '.txt' in line:
+                found_list.append("found python file: " + line.strip())
+            if '.txt' in line:
+                if verbose:
                     print("found txt file")
+                found_list.append("found txt file" + line.strip())
 
         print('Lines: ' + str(line_num))
+
+    print("Found List")
+    for item in found_list:
+        print("-item: " + item)
 
     print('Run: ', end='')
     print(x + 1)
